4c8ea8e5932df21b2ce69139cee1ac34146d91e3
duduzinho001 <163134713+duduzinho001@users.noreply.github.com>
2025-08-12 11:57:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ app.post("/usuarios", (req, res)=>{
     usuarios.push(usuario)
     res.send("Usuário cadastrado com sucesso!")
     })
+
+    const produtos = []
+app.post("/produtos", (req, res)=>{
+    const produto = req.body
+    produtos.push(produto)
+    res.send("Prudoto registrado com sucesso!")
+    })
+
+    app.get("/produtos", (req, res)=>{
+        res.send(produtos)
+    })
+
 app.get("/usuarios", (req, res)=>{
     res.send(usuarios)
 })
